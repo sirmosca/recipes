8e17ed8d9ca163b7c877fa50bd3a6bd893a21aab
sirmosca <mosca125@gmail.com>
2014-07-13 17:54:35 -0400

    add unit tests for MainViewModel.cs

---
+++ Recipes/Recipes.Tests/MainViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recipes.ViewModel;
+
+namespace Recipes.Tests
+{
+    [TestClass]
+    public class MainViewModelTests
+    {
+        [TestMethod]
+        public void TestCanCreateViewModel()
+        {
+            MainViewModel vm = new MainViewModel();
+            Assert.IsNotNull(vm);
+        }
+    }
+}
