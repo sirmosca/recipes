a5953121733a6da75a51ba1467ab69c5bbae8cd5
sirmosca <mosca125@gmail.com>
2014-07-16 22:52:29 -0400

    view to add new recipe name, notes, serving size

---
+++ Recipes/Recipes/ViewModel/AddNewRecipeNameViewModel.cs
@@ -0,0 +1,56 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using Recipes.Message;
+using Recipes.Model;
+using Recipes.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Recipes.ViewModel
+{
+    public class AddNewRecipeNameViewModel : ViewModelBase
+    {
+        private ICommand _save;
+        private ICommand _cancel;
+
+        public AddNewRecipeNameViewModel()
+        {
+            _save = new RelayCommand(OnSave);
+            _cancel = new RelayCommand(OnCancel);
+        }
+
+        private void OnSave()
+        {
+            try
+            {
+                var repo = new RecipeRepository();
+                repo.Save(Name, Notes, ServingSize);
+            }
+            catch (Exception ex)
+            {
+                //log here
+            }
+        }
+
+        private void OnCancel()
+        {
+            Messenger.Default.Send(new CancelAddNewRecipeNameMessage());
+        }
+
+        public ICommand Cancel { get { return _cancel; } }
+
+        public ICommand Save { get { return _save; } }
+        
+        public string Name { get; set; }
+
+        public string Notes { get; set; }
+
+        public string ServingSize { get; set; }
+    }
+}
