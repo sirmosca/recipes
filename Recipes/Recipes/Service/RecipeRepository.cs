90fc2a4f826d82778ccaa65c33357cb8e1a3e4c6
sirmosca <mosca125@gmail.com>
2014-07-16 22:55:14 -0400

    add repo for saving recipes

---
+++ Recipes/Recipes/Service/RecipeRepository.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Recipes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.Service
+{
+    public class RecipeRepository
+    {
+        public void Save(string name, string notes, string servingSize)
+        {
+            Recipe r = new Recipe();
+            r.Name = name;
+            r.Notes = notes;
+            r.ServingSize = servingSize;
+
+            using (var context = new  RecipesEntities())
+            {
+                r = context.Recipes.Add(r);
+                var b = context.GetValidationErrors();
+            }
+        }
+    }
+}
